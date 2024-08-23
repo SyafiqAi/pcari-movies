244c713d96f795a8fe2aa43b23268d1aaf10bce3
Syafiq Aiman <isyafiq@gmail.com>
2024-08-23 23:57:49 +0800

    movies route

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Http\Controllers\Api\V1\MovieController;
+use App\Models\Performer;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1'], function() {
+    Route::apiResource('movies', MovieController::class);
+});
