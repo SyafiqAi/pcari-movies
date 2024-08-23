b8e3c3bb7cb5caf53f7e6c0a40131a36d2d102ea
Syafiq Aiman <isyafiq@gmail.com>
2024-08-23 20:20:15 +0800

    create all tables

--- database/migrations/2024_08_23_112017_create_movies_table.php
+++ database/migrations/2024_08_23_112017_create_movies_table.php
@@ -12,7 +12,18 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('movies', function (Blueprint $table) {
-            $table->id();
+            $table->id('movie_id');
+            $table->string('title');
+            $table->date('release');
+            $table->integer('length'); // length of the movie in minutes.
+            $table->string('description');
+            $table->string('mpaa_rating');
+            $table->string('director');
+            $table->string('language');
+            $table->string('theater_name');
+            $table->integer('theater_room_no');
+            $table->dateTime('start_time');
+            $table->dateTime('end_time');
             $table->timestamps();
         });
     }
--- database/migrations/2024_08_23_112127_create_user_ratings_table.php
+++ database/migrations/2024_08_23_112127_create_user_ratings_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('user_ratings', function (Blueprint $table) {
             $table->id();
+            $table->integer('movie_id');
+            $table->string('username');
+            $table->integer('rating');
             $table->timestamps();
         });
     }
