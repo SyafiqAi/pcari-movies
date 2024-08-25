a9323470003a0a787776c5f85b2e2146af60dd99
Syafiq Aiman <isyafiq@gmail.com>
2024-08-25 12:44:26 +0800

    fix add_movie route

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1'], f
     Route::get('specific_movie_theater', [MovieController::class, 'index']);
     Route::get('search_performer', [MovieController::class, 'index']);
     Route::get('new_movies', [MovieController::class, 'index']);
-    Route::post('add_movie/{syafiq}', [MovieController::class, 'store']);
+    Route::post('add_movie', [MovieController::class, 'store']);
     Route::apiResource('movies', MovieController::class);
     #endregion
 
