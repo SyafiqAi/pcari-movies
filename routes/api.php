5b30dbd496e549791380a0f8a4f3e28be2b414c2
Syafiq Aiman <isyafiq@gmail.com>
2024-08-25 12:30:43 +0800

    custom routes

--- routes/api.php
+++ routes/api.php
@@ -10,7 +10,18 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1'], function() {
+Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1'], function () {
+    #region movies
+    Route::get('genre', [MovieController::class, 'index']);
+    Route::get('timeslot', [MovieController::class, 'index']);
+    Route::get('specific_movie_theater', [MovieController::class, 'index']);
+    Route::get('search_performer', [MovieController::class, 'index']);
+    Route::get('new_movies', [MovieController::class, 'index']);
+    Route::post('add_movie/{syafiq}', [MovieController::class, 'store']);
     Route::apiResource('movies', MovieController::class);
-    Route::post('rate', [UserRatingController::class, 'store']);
+    #endregion
+
+    #region rating
+    Route::post('give_rating', [UserRatingController::class, 'store']);
+    #endregion
 });
